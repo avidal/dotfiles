a2f4d0691f37ff327c2039d405f4da83699c320b
Alex Vidal <alex.vidal@gmail.com>
2020-01-22 15:44:09 -0600

    fish: add tldr function for cht.sh lookups

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,13 @@
 set poetry_bin $HOME/.poetry/bin
 set rust_bin $HOME/.cargo/bin
 set go_bin $HOME/go/bin
+set lua_bin $HOME/.luarocks/bin
 set home_path $HOME/.local/bin:$HOME/bin
 set local_path /usr/local/bin:/usr/local/sbin
 
-set -gx PATH $rust_bin $go_bin $poetry_bin $home_path $local_path $PATH
+set -gx PATH $rust_bin $go_bin $poetry_bin $lua_bin $home_path $local_path $PATH
 set -gx GO111MODULE on
+set -gx GOPRIVATE 'code.cfops.it'
 
 if type -q direnv
     direnv hook fish | source
