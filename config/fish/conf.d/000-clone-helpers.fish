0b51f2c93c6d84946b2bc98a9145318416bc7526
Alex Vidal <alex.vidal@gmail.com>
2020-03-03 14:19:30 -0600

    fish: add clone-work alias for quick-clone of khan repositories

--- config/fish/conf.d/000-clone-helpers.fish
+++ config/fish/conf.d/000-clone-helpers.fish
@@ -9,5 +9,11 @@ function clone-github --description "clone-github <user/repo>"
     git clone git@github.com:$repo $HOME/Code/github.com/$repo $argv
 end
 
+function clone-work --description "clone-work <repo>"
+    set repo $argv[1]; and set --erase argv[1];
+    git clone git@github.com:Khan/$repo $HOME/Code/github.com/Khan/$repo $argv
+    git config --file $HOME/Code/github.com/Khan/$repo/.git/config --add user.email 'alexvidal@khanacademy.org'
+end
+
 # no file completions for these commands
 complete -f -c clone-github -c clone-git
