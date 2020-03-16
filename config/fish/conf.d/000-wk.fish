5efef57f7e425308119c96fb08a17ca7b3e9c546
Alex Vidal <alex@heyviddy.com>
2020-03-16 11:15:14 -0500

    fish: update wk alias to support leading tilde for git.sr.ht support

--- config/fish/conf.d/000-wk.fish
+++ config/fish/conf.d/000-wk.fish
@@ -4,14 +4,15 @@
 set CODE $HOME/Code
 
 function wk
-    cd $CODE/$argv
+    cd $CODE/$argv 2>/dev/null || cd $CODE/\~$argv 2>/dev/null
 end
 
 function _wk_complete
     # find all owner/repo folders in $CODE/
     # and strip the $CODE/ path components to clean it up
+    # as well as the leading ~ from git.sr.ht
     find $CODE -mindepth 2 -maxdepth 2 -type d | \
-        sed -E "s:^$CODE/::" 2>/dev/null
+        sed -E "s:^$CODE/~?::" 2>/dev/null
 end
 
 complete -c wk -f -a '(_wk_complete)'
